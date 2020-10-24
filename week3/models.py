306d1896b4d8f9bad607f888e83232b35d07d19b
langat justus <justuslangat78@gmail.com>
2020-10-24 18:00:31 +0300

    added profile and projects classes and make migrations

--- week3/models.py
+++ week3/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch  import receiver
+from django.http import Http404
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.png', upload_to='profile_pics/')
+    bio = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    class Meta:
+        db_table = 'profile'
+
+    @receiver(post_save, sender=User)
+    def update_create_profile(sender,instance,created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+
+    def save_profile(self):
+        self.save()
+
+
+
+class Projects(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    image = models.ImageField(upload_to='profile_pics/')
+    description = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=255)
+    link = models.URLField()
+    author_profile = models.ForeignKey(Profile, on_delete=models.CASCADE, default='1', blank = True)
+
+    def save_project(self):
+        self.save()
+
+    def __str__(self):
+        return f'{self.author} Post'
+
+    class Meta:
+        db_table = 'project'
+        ordering = ['-created_date']
+
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def search_projects(cls,search_term):
+        project = cls.objects.filter(title__icontains=search_term)
+        return project
+
+    @classmethod
+    def get_project(cls,id):
+        try:
+            project = Projects.objects.get(pk=id)
+        except ObjectDoesNotExist:
+            raise Http404()
+        return Project
