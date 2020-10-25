9bfa6ad827d2e83c4c2ce5ff44e1bec40141a07c
langat justus <justuslangat78@gmail.com>
2020-10-25 10:42:18 +0300

    added index html file and updated the urls

--- week3/urls.py
+++ week3/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('accounts/register/', views.registration, name='register'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('accounts/profile/', views.profile, name='profile'),
+    path('updateprofile/', views.updateprofile, name='updateprofile'),
 ]
