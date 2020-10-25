fa2254c023e16d9516edcb63693dba520561a2af
langat justus <justuslangat78@gmail.com>
2020-10-25 09:28:11 +0300

    added project html file

--- week3/forms.py
+++ week3/forms.py
@@ -9,3 +9,15 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image', 'bio']        
