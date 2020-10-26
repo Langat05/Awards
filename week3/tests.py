b122df96a3407d268b5783c21ef097018655b617
langat justus <justuslangat78@gmail.com>
2020-10-26 16:05:17 +0300

    write the test for the profile class

--- week3/tests.py
+++ week3/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user('langat')
+        cls.profile1 = Profile(profile_pics = 'bab.jpg', bio = 'born in Kenya', user = cls.user)
+
+    def test_instance(cls):
+        cls.assertTrue(isinstance(cls.profile1, Profile))
+
+    def save_method_test(self):
+        self.profile1.save_profile()
+        images = Image.object.all()
+        self.assertTrue(len(images)>0)    
